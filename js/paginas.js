50f257399ba728ed5412887bad4d2d60ea5d99f4
Omar Noyola <omis1296@gmail.com>
2022-09-06 17:43:23 -0500

    :zap: Add Página09

--- js/paginas.js
+++ js/paginas.js
@@ -1,7 +1,7 @@
 const paginas = [];
 
 function generarPaginas(){
-    const totalPaginas = 22;
+    const totalPaginas = 9;
 
     for(let contador = 1; contador<=totalPaginas; contador++){
         let nombrePagina;
